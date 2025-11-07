edd3b44d2509285a0f9e38f9bab31c2660bb0830
KMM05XAK20 <marat.kozhenkov@gmail.com>
2025-11-07 21:00:23 +0300

    debug: fixed the error:: ImportError: cannot import name 'ParseMode' from 'aiogram.types' by rolling back main to the previous commit

--- bot/main.py
+++ bot/main.py
@@ -22,12 +22,15 @@ async def main():
 
     dp.include_router(root_router)
 
+
     if settings.use_webhook:
         print("Webhook mode включен — пропуск опроса.")
         return
     else:
         await bot.delete_webhook(drop_pending_updates=True)
         await dp.start_polling(bot)
+
+    #await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
 
 if __name__ == "__main__":
     asyncio.run(main())
